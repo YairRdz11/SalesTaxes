5bbef1cdf85a9c451cbd70d1c75598b677af2a8e
Yair <yrodriguez@aumenta.mx>
2021-04-08 16:45:33 -0500

    Create class worehouse

---
+++ Models/Warehouse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Models
+{
+    public class Warehouse
+    {
+        public IList<Product> ProductList { get; }
+
+        public Warehouse()
+        {
+            ProductList = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            if(ProductList.Any(x=> x.Equals(product))) return;
+            ProductList.Add(product);
+        }
+
+        public string PrintList()
+        {
+            var cad = string.Empty;
+            foreach (var product in ProductList)
+            {
+                cad += product.ToString();
+                Console.WriteLine(product.ToString());
+            }
+
+            return cad;
+        }
+    }
+}
